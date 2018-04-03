f7451a4cab2a41943e1a2cc706fd17abf2c12f6c
MightyMaxim <37951439+MightyMaxim@users.noreply.github.com>
2018-04-03 08:32:05 -0700

    Ver 1.0
    ver 1.0

---
+++ DSCLCM_HTTPSPull.ps1
@@ -0,0 +1,62 @@
+﻿# Copyright 2018, Maksim Krupnov
+#
+# Version 1.0
+# 
+
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $RegKey,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $DSCPullServerName,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $NodeName,  
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Path,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [array]  $ConfigNames
+)
+
+[DSCLocalConfigurationManager()]
+
+Configuration LCM_HTTPSPULL 
+{
+	Node $NodeName {
+	
+		Settings {
+        
+        	AllowModuleOverwrite = $True
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+			RefreshMode = 'Pull'
+
+        }
+ 
+        ConfigurationRepositoryWeb PullServer {
+            ServerURL = "https:// + $DSCPullServerName + :8080/PSDSCPullServer.svc"
+#for HTTP   ServerURL = "http:// + $DSCPullServerName + :8080/PSDSCPullServer.svc"
+            RegistrationKey = $RegKey
+            ConfigurationNames = $ConfigNames 
+            AllowUnsecureConnection = $false
+#for HTTP   AllowUnsecureConnection = $true
+        }
+
+        ResourceRepositoryWeb PullServerModules {
+            ServerURL = "https://" + $DSCPullServerName + ":8080/PSDSCPullServer.svc"
+#for HTTP   ServerURL = "http:// + $DSCPullServerName + :8080/PSDSCPullServer.svc"
+            RegistrationKey = $RegKey
+            AllowUnsecureConnection = $false
+#for HTTP   AllowUnsecureConnection = $true
+        }
+	}
+}
+
+# Generate Meta.Mof in folder
+LCM_HTTPSPULL -OutputPath $Path
---
+++ DSCPullServerConfig.ps1
@@ -0,0 +1,69 @@
+﻿# Copyright 2018, Maksim Krupnov
+#
+# Version 1.0
+# 
+
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Cert,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $NodeName,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Path
+
+)
+
+configuration PullServerConfig
+{
+    # Modules must exist on target pull server
+    Import-DSCResource -ModuleName xPSDesiredStateConfiguration
+
+    Node $NodeName {
+        
+        WindowsFeature DSCServiceFeature {
+            Ensure = "Present"
+            Name   = "DSC-Service"
+        }
+# Only for GUI
+#        WindowsFeature IISConsole {
+#            Ensure = "Present"
+#            Name   = "Web-Mgmt-Console"
+#        }
+
+        xDscWebService PSDSCPullServer {
+            Ensure                  = "Present"
+            EndpointName            = "PSDSCPullServer"
+            Port                    = 8080
+            PhysicalPath            = "$env:SystemDrive\inetpub\wwwroot\PSDSCPullServer"
+            CertificateThumbPrint   = $Cert
+#for HTTP   CertificateThumbPrint   = "AllowUnencryptedTraffic"
+            ModulePath              = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules"
+            ConfigurationPath       = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Configuration"
+            RegistrationKeyPath     = "$env:PROGRAMFILES\WindowsPowerShell\DscService"   
+            AcceptSelfSignedCertificates = $true  
+#for HTTP   AcceptSelfSignedCertificates = $false
+            State                   = "Started"
+            DependsOn               = "[WindowsFeature]DSCServiceFeature"
+            UseSecurityBestPractices = $true
+#for HTTP   UseSecurityBestPractices = $false
+        }
+
+        File RegistrationKeyFile {
+        Ensure          = 'Present'
+        Type            = 'File'
+        DestinationPath = "$env:ProgramFiles\WindowsPowerShell\DscService\RegistrationKeys.txt"
+        }
+    }
+}
+
+
+PullServerConfig -OutputPath $path
+
+New-DSCChecksum ($path + $NodeName + '.mof') -FORCE
+
+
