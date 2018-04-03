77c2412d90da1ad4b253d2e6f31fa2c54b0965f7
Maksim Krupnov <37951439+MightyMaxim@users.noreply.github.com>
2018-04-03 13:44:42 -0700

    Ver 1.0
    DC Server Configuration

---
+++ DSCDCServerConfig.ps1
@@ -0,0 +1,49 @@
+﻿# Copyright 2018, Maksim Krupnov
+#
+# Version 1.0
+# 
+
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $NodeName,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Path
+
+)
+
+configuration DCServerConfig
+{
+    # Modules must exist on target pull server
+ #   Import-DSCResource -ModuleName xActiveDirectory
+
+    Node $NodeName {
+        
+        File ADFiles            
+        {            
+            DestinationPath = 'C:\NTDS'            
+            Type = 'Directory'            
+            Ensure = 'Present'            
+        }            
+                    
+        WindowsFeature ADDSInstall             
+        {             
+            Ensure = "Present"             
+            Name = "AD-Domain-Services"             
+        }            
+            
+        # Optional GUI tools            
+        WindowsFeature ADDSTools            
+        {             
+            Ensure = "Present"             
+            Name = "RSAT-ADDS"             
+        }            
+    }
+}
+
+
+DCServerConfig -OutputPath $path
+
+New-DSCChecksum ($path + $NodeName + '.mof') -FORCE
